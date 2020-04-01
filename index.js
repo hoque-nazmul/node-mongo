2915b0e0dbec4987a2c3e164b963c3de140be1cb
Nazmul Hoque <coder.nazmul@gmail.com>
2020-04-01 23:39:59 +0600

    product insert & find done

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
-const port = 3000
+const port = process.env.PORT;
+const dbUser = process.env.DB_USER;
+const password = process.env.DB_PASS;
+const uri = `mongodb+srv://${dbUser}:${password}@cluster0-nrdl2.mongodb.net/test?retryWrites=true&w=majority `;
+let client = new MongoClient(uri, { useNewUrlParser: true });
 
 const users = [
     {id: '1', name: "Nazmul Hoque", age : 25, salary : 15000},
@@ -15,8 +21,24 @@ const users = [
 
 const usernames = ["Towhid", "Khalik", "Jhankar", "Mukta", "Anayet", "Nazmul"]
 
-app.get('/', (req, res) => {
-    res.send('App is editing....')
+
+app.get('/products', (req, res) => {
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.find().toArray((err, documents) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send({massage:err});
+            }
+            else{
+                console.log("Data find successfully.....", documents);
+                res.send(documents);
+            }
+           
+        })
+        client.close();
+    });
 })
 
 app.get('/users', (req, res) => {
@@ -38,11 +60,34 @@ app.get('/user/:id', (req, res) => {
 });
 
 // Post Request
-app.post('/addUser', (req, res) => {
-    const user = req.body;
-    res.send(user);
-    // console.log(req.body);
-    // res.send('POST request to the homepage')
+app.post('/addProduct', (req, res) => {
+    const product = req.body;
+    client = new MongoClient(uri, { useNewUrlParser: true })
+    client.connect(err => {
+        const collection = client.db("onlineStore").collection("products");
+        collection.insertOne(product, (err, result) => {
+            if(err) {
+                console.log(err);
+                res.status(500).send({massage:err});
+            }
+            else{
+                console.log("Data Inserted Successfully....", result);
+                res.send(result.ops[0]);
+            }
+        })
+        client.close();
+    });
 })
 
-app.listen(port, () => console.log(`listening on port ${port}!`))
+function TimeFormatter(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0'+minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+app.listen(port, () => console.log(`listening on port ${port}! Execution Time: ${TimeFormatter(new Date)}`))
